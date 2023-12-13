aa4baea6e3697c9bfe1d2e837abcad76b2e0102b
Gilles Querret <g.querret@riverside-software.fr>
2023-12-13 11:31:14 +0100

    Local build script

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@ set JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-17.0.4.8-hotspot
 set JAVA_HOME_11_X64=C:\Program Files\Eclipse Adoptium\jdk-11.0.14.101-hotspot
 set JAVA_HOME_17_X64=C:\Program Files\Eclipse Adoptium\jdk-17.0.4.8-hotspot
 
-mvn --toolchains .azure-pipelines/toolchains.xml -Dmaven.test.skip=true -DARCHITECT_P2_DIR=/C:/Progress/P2 clean package
+mvn --toolchains .cirrus/toolchains.xml -Dmaven.test.skip=true -DARCHITECT_P2_DIR=/C:/Progress/P2 clean package
